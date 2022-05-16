3a852b4af0cee3c9603786547cc82e68fd487753
Maksim <71653309+IMaxikI@users.noreply.github.com>
2022-05-16 18:01:17 +0300

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ let products= [
     // jackets
     {
     "name": "Columbia Ascender Soft shell",
-    "description": "Крутое описание куртки.",
+    "description": "Мужская ветровка из ткани Softshell подойдет для походов.",
     "price": "59.99",
     "colors": {
         "black": "assets/products/jackets-m/ColumbiaAscenderSoftshell-black.jpg",
@@ -14,7 +14,7 @@ let products= [
     },
     {
         "name": "Kappa jacket",
-        "description": "Крутое описание куртки.",
+        "description": "Ветровка Kappa с оригинальным дизайном — отличный выбор для ненастных дней.",
         "price": "89.99",
         "colors": {
             "black": "assets/products/jackets-m/Kappa-black.jpg",
@@ -25,7 +25,7 @@ let products= [
     },
     {
         "name": "Nike Run Stripe",
-        "description": "Крутое описание куртки.",
+        "description": "Беги навстречу ветру в Nike Run Stripe. Легкая ветровка станет отличным выбором для тренировки в прохладную погоду.",
         "price": "79.99",
         "colors": {
             "black": "assets/products/jackets-m/NikeRunStripe-black.jpg",
@@ -36,7 +36,7 @@ let products= [
     },
     {
         "name": "Columbia Park 2",
-        "description": "Крутое описание куртки.",
+        "description": "Ветровка Columbia для комфортного отдыха на природе и походов.",
         "price": "49.99",
         "colors": {
             "white": "assets/products/jackets-w/ColumbiaPark2-white.jpg",
@@ -48,7 +48,7 @@ let products= [
     },
     {
         "name": "Nike Dri-FIT",
-        "description": "Крутое описание куртки.",
+        "description": "Раскрой свой потенциал вместе с Nike Dri-FIT! Технологичная ветровка с функциональным дизайном сделает твои пробежки в прохладную погоду комфортнее.",
         "price": "55.99",
         "colors": {
             "black": "assets/products/jackets-w/NikeDriFIT-black.jpg",
@@ -59,7 +59,7 @@ let products= [
     }, // shirts
     {
         "name": "Nike Pro Dri",
-        "description": "Крутое описание футболки.",
+        "description": "Легкая футболка Nike Pro для комфорта в самые жаркие моменты тренировки.",
         "price": "16.99",
         "colors": {
             "black": "assets/products/shirts-m/NikeProDri-black.jpg",
@@ -71,7 +71,7 @@ let products= [
     },
     {
         "name": "Puma ESS M",
-        "description": "Крутое описание футболки.",
+        "description": "Футболка с вышитым логотипом PUMA на груди для яркого летнего образа в спортивном стиле.",
         "price": "26.99",
         "colors": {
             "black": "assets/products/shirts-m/PumaESS-black.jpg",
@@ -83,7 +83,7 @@ let products= [
     },
     {
         "name": "Puma RAD",
-        "description": "Крутое описание футболки.",
+        "description": "Стильная футболка с логотипом PUMA для запоминающегося образа в спортивном стиле.",
         "price": "39.99",
         "colors": {
             "black": "assets/products/shirts-m/PumaRAD-black.jpg",
@@ -95,7 +95,7 @@ let products= [
     },
     {
         "name": "Termit",
-        "description": "Крутое описание футболки.",
+        "description": "Принтованная футболка Termit отлично впишется в летний образ.",
         "price": "39.99",
         "colors": {
             "black": "assets/products/shirts-m/Termit-black.jpg",
@@ -108,7 +108,7 @@ let products= [
     },
     {
         "name": "Nike Dri",
-        "description": "Крутое описание футболки.",
+        "description": "Теннисная футболка Nike Court Dri для чувства прохлады даже во время самых напряженных матчей.",
         "price": "16.99",
         "colors": {
             "white": "assets/products/shirts-w/NikeDri-white.jpg"
@@ -119,7 +119,7 @@ let products= [
     },
     {
         "name": "Nike Sportwear",
-        "description": "Крутое описание футболки.",
+        "description": "Хлопковая футболка Nike с вышитым логотипом дополнит гардероб в спортивном стиле.",
         "price": "16.99",
         "colors": {
             "black": "assets/products/shirts-w/NikeSportwear-balck.jpg",
@@ -131,7 +131,7 @@ let products= [
     },
     {
         "name": "Puma ESS W",
-        "description": "Крутое описание футболки.",
+        "description": "Футболка в спортивном стиле, декорированная принтом с логотипом PUMA.",
         "price": "26.99",
         "colors": {
             "white": "assets/products/shirts-w/PumaESS-white.jpg",
@@ -143,7 +143,7 @@ let products= [
     },
     {
         "name": "Puma Power Tape",
-        "description": "Крутое описание футболки.",
+        "description": "Укороченная футболка PUMA, декорированная принтом с логотипом бренда, поможет создать стильный спортивный образ.",
         "price": "26.99",
         "colors": {
             "black": "assets/products/shirts-w/PumaPowerTape-black.jpg",
@@ -155,7 +155,7 @@ let products= [
     },// Shoes
     {
         "name": "Adidas Strutter",
-        "description": "Крутое описание кроссовок.",
+        "description": "Кроссовки adidas Strutter с массивной подошвой позволят создать оригинальный образ в спортивном стиле. Модель гарантирует максимальный комфорт на протяжении всего дня.",
         "price": "36.99",
         "colors": {
             "white": "assets/products/shoes-m/AdidasStrutter-white.jpg"
@@ -166,7 +166,7 @@ let products= [
     },
     {
         "name": "Nike Defy All Day",
-        "description": "Крутое описание кроссовок.",
+        "description": "В кроссовках Nike Defy All Day ты можешь не бояться вызовов, которые приготовил для тебя новый день. Комфортная модель с продуманным дизайном — идеальный вариант для универсального образа в спортивном стиле.",
         "price": "16.99",
         "colors": {
             "black": "assets/products/shoes-m/NikeDefyAllDay-black.jpg",
@@ -178,7 +178,7 @@ let products= [
     },
     {
         "name": "Puma X-Ray",
-        "description": "Крутое описание кроссовок.",
+        "description": "Удобные кроссовки X-Ray проникнуты идеей скорости. Они способны преобразить любой лук, сделав его стильным и динамичным.",
         "price": "26.99",
         "colors": {
             "black": "assets/products/shoes-m/PumaX-Ray-black.jpg",
@@ -190,7 +190,7 @@ let products= [
     },
     {
         "name": "Nike Wearallday",
-        "description": "Крутое описание кроссовок.",
+        "description": "Nike Wearallday — это воплощение комфорта. Кроссовки полностью соответствуют своему названию — тебе будет в них комфортно на протяжении всего дня.",
         "price": "26.99",
         "colors": {
             "black": "assets/products/shoes-w/NikeWearallday-black.jpg"
@@ -201,7 +201,7 @@ let products= [
     },
     {
         "name": "Puma Runner",
-        "description": "Крутое описание кроссовок.",
+        "description": "Классический силуэт кроссовок Runner, дополненный функциональной отделкой задника, не оставит равнодушным истинного ценителя спортивного стиля.",
         "price": "26.99",
         "colors": {
             "black": "assets/products/shoes-w/PumaRunner-black.jpg",
@@ -213,7 +213,7 @@ let products= [
     },// Sport-products
     {
         "name": "Гантель Kettler 15кг",
-        "description": "Крутое описание товара.",
+        "description": "Гантели весом 15 кг — оптимальный выбор для силовых упражнений и функциональных тренировок. Грузы имеют шестиугольную форму и покрытие из высокопрочного синтетического каучука, что обеспечивает стабильность гантели как на ровной, так и на наклонной поверхности пола.",
         "price": "312.00",
         "colors": {
             "black": "assets/products/sport-products/Dumbbell-kettler-15.jpg"
@@ -224,7 +224,7 @@ let products= [
     },
     {
         "name": "Гантель Demix 5кг",
-        "description": "Крутое описание товара.",
+        "description": "Гантель эргономичной формы со скругленными краями. Предназначена для безопасного использования и хранения в домашних условиях. Нескользящее неопреновое покрытие обеспечивает надежный хват гантелей во время занятий и амортизацию при соприкосновении с полом.",
         "price": "98.80",
         "colors": {
             "black": "assets/products/sport-products/Dumbbell-demix-5.jpg"
@@ -235,7 +235,7 @@ let products= [
     },
     {
         "name": "Гантель Demix 3кг",
-        "description": "Крутое описание товара.",
+        "description": "Гантель эргономичной формы со скругленными краями. Предназначена для безопасного использования и хранения в домашних условиях. Нескользящее неопреновое покрытие обеспечивает надежный хват гантелей во время занятий и амортизацию при соприкосновении с полом.",
         "price": "57.80",
         "colors": {
             "red": "assets/products/sport-products/Dumbbell-demix-3.jpg"
@@ -246,7 +246,7 @@ let products= [
     },
     {
         "name": "Беговая дорожка Olympia",
-        "description": "Крутое описание товара.",
+        "description": "Технологичная беговая дорожка Torneo — отличный выбор для комфортных и эффективных домашних тренировок.",
         "price": "2996.99",
         "colors": {
             "black": "assets/products/sport-products/Treadmill-olympia.jpg"
@@ -257,7 +257,7 @@ let products= [
     },
     {
         "name": "Беговая дорожка Torneo Nota",
-        "description": "Крутое описание товара.",
+        "description": "Беговая дорожка Torneo предоставляет расширенные возможности для эффективных домашних тренировок на мышцы ног и пресса.",
         "price": "2245.59",
         "colors": {
             "black": "assets/products/sport-products/Treadmill-torneoNota.jpg"
@@ -268,7 +268,7 @@ let products= [
     },
     {
         "name": "Велосипед горный Stern Energy 27,5",
-        "description": "Крутое описание товара.",
+        "description": "Велосипед Energy с алюминиевой рамой — отличный выбор для поклонников активного отдыха.",
         "price": "1105.00",
         "colors": {
             "black": "assets/products/sport-products/bike-stern-energy-27,5'.jpg"
@@ -279,7 +279,7 @@ let products= [
     },
     {
         "name": "Велосипед горный Dynamic 26",
-        "description": "Крутое описание товара.",
+        "description": "Горный велосипед Stern станет удачным выбором для новичков в велоспорте.",
         "price": "972.00",
         "colors": {
             "black": "assets/products/sport-products/bike-stern-dynamic-26'.jpg"
@@ -290,7 +290,7 @@ let products= [
     },
     {
         "name": "Велотренажер магнитный NordicTrack GX 2.7 U",
-        "description": "Крутое описание товара.",
+        "description": "Велотренажер NordicTrack GX 2.7 U для эффективных и комфортных тренировок.",
         "price": "1678.00",
         "colors": {
             "black": "assets/products/sport-products/BikeTr-nordicTrack-gx-u.jpg"
@@ -301,7 +301,7 @@ let products= [
     },
     {
         "name": "Велотренажер магнитный Kettler Axos Cycle M",
-        "description": "Крутое описание товара.",
+        "description": "Простой и надежный велотренажер отлично подходит для кардиотренировок. Занятия укрепляют мышцы ног и пресса и повышают аэробную выносливость. Широкое и мягкое прорезиненное сиденье обеспечивает комфорт во время занятий.",
         "price": "1020.00",
         "colors": {
             "gray": "assets/products/sport-products/BikeTr-kettler-axos-cycle-m.jpg"
